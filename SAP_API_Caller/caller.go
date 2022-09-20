0f31afafc3f8da8dbc1ead1243a954ff41d55c80
Yurino Oga <yurino.o@latona.io>
2022-09-20 19:40:36 +0900

    release

---
+++ SAP_API_Caller/caller.go
@@ -0,0 +1,170 @@
+package sap_api_caller
+
+import (
+	"fmt"
+	"io/ioutil"
+	sap_api_output_formatter "sap-api-integrations-production-order-reads/SAP_API_Output_Formatter"
+	"strings"
+	"sync"
+
+	sap_api_request_client_header_setup "github.com/latonaio/sap-api-request-client-header-setup"
+
+	"github.com/latonaio/golang-logging-library-for-sap/logger"
+)
+
+type SAPAPICaller struct {
+	baseURL         string
+	sapClientNumber string
+	requestClient   *sap_api_request_client_header_setup.SAPRequestClient
+	log             *logger.Logger
+}
+
+func NewSAPAPICaller(baseUrl, sapClientNumber string, requestClient *sap_api_request_client_header_setup.SAPRequestClient, l *logger.Logger) *SAPAPICaller {
+	return &SAPAPICaller{
+		baseURL:         baseUrl,
+		requestClient:   requestClient,
+		sapClientNumber: sapClientNumber,
+		log:             l,
+	}
+}
+
+func (c *SAPAPICaller) AsyncGetProductionOrder(manufacturingOrder string, accepter []string) {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(accepter))
+	for _, fn := range accepter {
+		switch fn {
+		case "General":
+			func() {
+				c.General(manufacturingOrder)
+				wg.Done()
+			}()
+		default:
+			wg.Done()
+		}
+	}
+
+	wg.Wait()
+}
+
+func (c *SAPAPICaller) General(manufacturingOrder string) {
+	generalData, err := c.callProductionOrderSrvAPIRequirementGeneral("A_ProductionOrder_2", manufacturingOrder)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(generalData)
+
+	componentData, err := c.callToComponent(generalData[0].ToComponent)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(componentData)
+
+	itemData, err := c.callToItem(generalData[0].ToItem)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(itemData)
+
+	operationData, err := c.callToOperation(generalData[0].ToOperation)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(operationData)
+
+	statusData, err := c.callToStatus(generalData[0].ToStatus)
+	if err != nil {
+		c.log.Error(err)
+		return
+	}
+	c.log.Info(statusData)
+}
+
+func (c *SAPAPICaller) callProductionOrderSrvAPIRequirementGeneral(api, manufacturingOrder string) ([]sap_api_output_formatter.General, error) {
+	url := strings.Join([]string{c.baseURL, "API_PRODUCTION_ORDER_2_SRV", api}, "/")
+	param := c.getQueryWithGeneral(map[string]string{}, manufacturingOrder)
+
+	resp, err := c.requestClient.Request("GET", url, param, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToGeneral(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToComponent(url string) ([]sap_api_output_formatter.ToComponent, error) {
+	resp, err := c.requestClient.Request("GET", url, map[string]string{}, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToToComponent(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToItem(url string) ([]sap_api_output_formatter.ToItem, error) {
+	resp, err := c.requestClient.Request("GET", url, map[string]string{}, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToToItem(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToOperation(url string) ([]sap_api_output_formatter.ToOperation, error) {
+	resp, err := c.requestClient.Request("GET", url, map[string]string{}, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToToOperation(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) callToStatus(url string) ([]sap_api_output_formatter.ToStatus, error) {
+	resp, err := c.requestClient.Request("GET", url, map[string]string{}, "")
+	if err != nil {
+		return nil, fmt.Errorf("API request error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	data, err := sap_api_output_formatter.ConvertToToStatus(byteArray, c.log)
+	if err != nil {
+		return nil, fmt.Errorf("convert error: %w", err)
+	}
+	return data, nil
+}
+
+func (c *SAPAPICaller) getQueryWithGeneral(params map[string]string, manufacturingOrder string) map[string]string {
+	if len(params) == 0 {
+		params = make(map[string]string, 1)
+	}
+	params["$filter"] = fmt.Sprintf("ManufacturingOrder eq '%s'", manufacturingOrder)
+	return params
+}
